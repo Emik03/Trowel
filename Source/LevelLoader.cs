af779d2fa0321c3081964c4f1c368514883298c8
Emik <emik.cuber@gmail.com>
2026-04-27 11:03:17 +0200

    Add overload

--- Source/LevelLoader.cs
+++ Source/LevelLoader.cs
@@ -4,34 +4,38 @@ namespace Trowel;
 /// <summary>Travels to a specific level.</summary>
 public static class Go
 {
-    /// <summary>Enters the selected level.</summary>
+    /// <summary>Enters the level.</summary>
     /// <param name="level">The level to enter.</param>
+    [CLSCompliant(false)]
+    public static void To(AdvantureLevel level) => To(LevelType.Explore, (int)level);
+
+    /// <inheritdoc cref="To(AdvantureLevel)"/>
     [CLSCompliant(false)]
     public static void To(ChallengeLevel level) => To(LevelType.Challenge, (int)level);
 
-    /// <inheritdoc cref="To(ChallengeLevel)"/>
+    /// <inheritdoc cref="To(AdvantureLevel)"/>
     [CLSCompliant(false)]
     public static void To(ExploreLevel level) => To(LevelType.Explore, (int)level);
 
-    /// <inheritdoc cref="To(ChallengeLevel)"/>
+    /// <inheritdoc cref="To(AdvantureLevel)"/>
     [CLSCompliant(false)]
     public static void To(SkinLevel level) => To(LevelType.SkinLevel, (int)level);
 
-    /// <inheritdoc cref="To(ChallengeLevel)"/>
+    /// <inheritdoc cref="To(AdvantureLevel)"/>
     [CLSCompliant(false)]
     public static void To(SurvivalLevel level) => To(LevelType.Survival, (int)level);
 
-    /// <inheritdoc cref="To(ChallengeLevel)"/>
+    /// <inheritdoc cref="To(AdvantureLevel)"/>
     [CLSCompliant(false)]
     public static void To(TravelAdvanture level) => To(LevelType.TravelAdvanture, (int)level);
 
-    /// <summary>Enters the selected level.</summary>
+    /// <summary>Enters the level.</summary>
     /// <param name="type">The type of level to enter.</param>
-    /// <param name="level">The level index to enter.</param>
+    /// <param name="level">The level to enter.</param>
     [CLSCompliant(false)]
     public static void To(LevelType type, int level)
     {
         UIMgr.EnterMainMenu();
-        UIMgr.EnterGame(LevelType.SkinLevel, level);
+        UIMgr.EnterGame(type, level);
     }
 }
